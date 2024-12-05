fad311329e6fd8837949b061996f5c6e82be4775
franco43-free <75459430+franco43-free@users.noreply.github.com>
2024-12-05 18:06:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Sn5h3Q6T#qKf1wrKOrSMhz246Cnwri_CHLMSsUslLvnFQ_f-29UY", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "zmZGDLgC#yCOUnxx2ciThM-o9gLanunWbiNPGRkUAjd7m6QgL1CY", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
